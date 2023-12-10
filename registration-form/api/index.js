4baeef4711e4dfc708a3d0bd274610d626d20f61
user-k-8 <khanyisile842@gmail.com>
2023-12-11 00:42:33 +0200

    api

--- registration-form/api/index.js
+++ registration-form/api/index.js
@@ -3,18 +3,27 @@ const cors = require('cors');
 const bodyParser =require('body-parser');
 const db = require('./db')
 
-db.connect(()=>{
-    console.log('db connected')
-})
-
 const app = express();
 
-app.use(cors()); // Enable CORS with custom options
+app.use(cors({
+    origin: process.env.url
+})); // Enable CORS with custom options
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', process.env.url);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+
+    next();
+  });
 
 app.set("view engine", "ejs");
 app.use(express.static('public'));
 
 const PORT = 4000
+
+db.connect(()=>{
+    console.log('db connected')
+})
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
