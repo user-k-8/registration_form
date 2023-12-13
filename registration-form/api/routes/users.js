bc33fd9dda3514180d8e2f33b18999f15848a65c
user-k-8 <khanyisile842@gmail.com>
2023-12-13 13:40:31 +0200

    api

--- registration-form/api/routes/users.js
+++ registration-form/api/routes/users.js
@@ -11,14 +11,14 @@ const secret =process.env.secret;
 
 const verifyJwt = (req, res, next)=>{
 
-   const token = req.headers["access-token"]
+   const token = req.headers['access-token']
    if(!token){
-    return res.json("Token is needed. Please log in")
+    return res.json('Token is needed. Please log in')
    }
    else{
     jwt.verify(token,  secret, (err, decoded)=>{
       if(err){
-        return res.json("Not Authenticated")
+        return res.json('Not Authenticated')
       }else{
         next();
       }
@@ -28,7 +28,7 @@ const verifyJwt = (req, res, next)=>{
 
 router.get('/profile-info', verifyJwt, (req, res)=>{
 
-  return res.json("Authenticated")
+  return res.json('Authenticated')
 
 })
 
@@ -48,12 +48,13 @@ body('password').isLength({ min: 8 })
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
+    console.log(errors)
     return res.status(400).json({ errors: errors.array() });
  }
 
  const {username, email, password, rememberMe}= req.body
    //check for duplilcate usernames in db
-    const sql = `SELECT email  FROM users WHERE email = "${email}"`;
+    const sql = `SELECT email  FROM users WHERE email = '${email}'`;
     db.query(sql,  (err, result)=> {
       if (err) throw err;
      console.log(result);
@@ -64,15 +65,15 @@ body('password').isLength({ min: 8 })
       const hashedPwd = bcrypt.hash(password, 10);
     
     //store the new user
-        const sql = `INSERT INTO users (username, email, password, rememberMe) VALUES ( "${username}", "${email}", "${hashedPwd}", ${rememberMe})`;
+        const sql = `INSERT INTO users (username, email, password, rememberMe) VALUES ( '${username}', '${email}', '${hashedPwd}', ${rememberMe})`;
         db.query(sql,  (err, result)=> {
           if (err) throw err;
-          console.log("1 record inserted");
+          console.log('1 record inserted');
          
         });
     
      }else{
-      console.log("email already registered")
+      console.log('email already registered')
       return res.status(409).send({status: 409}); //Conflict 
      }
     
@@ -85,14 +86,14 @@ router.post('/login',  async (req, res)=>{
 
     const {email, password}= req.body
 
-    const sql = `SELECT email, password, user_id, username  FROM users WHERE email = "${email}"`;
+    const sql = `SELECT email, password, user_id, username  FROM users WHERE email = '${email}'`;
     db.query(sql, (err, result)=> {
           if (err) throw err;
           console.log(result)        
          
           if(!result[0]){
             console.log('User not found');
-            return res.status(404).send({status: "404"}); //User not found
+            return res.status(404).send({status: '404'}); //User not found
           }
 
          // evaluate password 
@@ -107,7 +108,7 @@ router.post('/login',  async (req, res)=>{
             return res.json({Login: true, token});    
         } 
         else {
-          return  res.status(401).send({status: "401"});//unauthorised
+          return  res.status(401).send({status: '401'});//unauthorised
         }
         });
 })
@@ -117,7 +118,7 @@ router.post('/googlelogin',  async (req, res)=>{
   const {username,email, rememberMe}= req.body
      
      //check for email
-      const sql = `SELECT email  FROM users WHERE email = "${email}"`;
+      const sql = `SELECT email  FROM users WHERE email = '${email}'`;
         db.query(sql,  (err, result)=> {
         if (err) throw err;
         console.log(result)
@@ -125,10 +126,10 @@ router.post('/googlelogin',  async (req, res)=>{
         if(!result[0]){
             console.log('User not registered in db');
            //store the new user
-           const sql = `INSERT INTO users (username, email,  rememberMe) VALUES ( "${username}", "${email}",  ${rememberMe})`;
+           const sql = `INSERT INTO users (username, email,  rememberMe) VALUES ( '${username}', '${email}',  ${rememberMe})`;
            db.query(sql,  (err, result)=> {
               if (err) throw err;
-              console.log("1 record inserted");
+              console.log('1 record inserted');
             });
         } 
             //generate token
@@ -141,14 +142,14 @@ router.post('/forgot-password', async(req,res)=>{
 
   const {email} =req.body;
 //check if user exists
-  const sql = `SELECT email, password, user_id  FROM users WHERE email = "${email}"`;
+  const sql = `SELECT email, password, user_id  FROM users WHERE email = '${email}'`;
   db.query(sql,  (err, result)=> {
   if (err) throw err;
 
   if(!result[0]){
       //user does not exist
       console.log('User not registered in db');
-      return res.send({message: "Account does not exist", status: 404});
+      return res.send({message: 'Account does not exist', status: 404});
   }
   else{
    // user exists
@@ -159,8 +160,8 @@ router.post('/forgot-password', async(req,res)=>{
     console.log(link)
     //send email
     const transporter = nodemailer.createTransport({
-     service: "gmail",
-     host: "smtp.gmail.com",
+     service: 'gmail',
+     host: 'smtp.gmail.com',
      port: 587,
      secure: false,
      auth: {
@@ -187,14 +188,14 @@ router.post('/forgot-password', async(req,res)=>{
        console.log('Email sent: ' + info.response);
      }
    });
-   return res.send({message: "Email sent", status: 200});
+   return res.send({message: 'Email sent', status: 200});
   }
 
 })
 })
 
 //get reset password form
-router.get("/reset-password/:user_id/:token", async(req,res)=>{
+router.get('/reset-password/:user_id/:token', async(req,res)=>{
 
   const {user_id, token} = req.params;
 
@@ -203,16 +204,16 @@ router.get("/reset-password/:user_id/:token", async(req,res)=>{
    const email = decodedToken.email;
 
     //render reset form
-    res.render("index", {email: email, status:"password not updated"})
+    res.render('index', {email: email, status:'password not updated'})
    
 }catch(error){
-  res.send("Not verified")
+  res.send('Not verified')
   console.log(error)
 }
 })
 
 //when user submits reset password form
-router.post("/reset-password/:user_id/:token", async(req,res)=>{
+router.post('/reset-password/:user_id/:token', async(req,res)=>{
 
   const {user_id, token} = req.params;
   const  {password} = req.body;
@@ -222,16 +223,16 @@ router.post("/reset-password/:user_id/:token", async(req,res)=>{
     const email = decodedToken.email;
     //update password
     const hashedPwd = await bcrypt.hash(password, 10);
-    const sql = `UPDATE users SET password = "${hashedPwd}" where email=${email}`;
+    const sql = `UPDATE users SET password = '${hashedPwd}' where email=${email}`;
     db.query(sql,  (err, result)=> {
       if (err) throw err;
-      console.log("1 record updated");
+      console.log('1 record updated');
     });
     //render page to direct users to login 
-    res.render("index", {email: email, status:"password updated"})
+    res.render('index', {email: email, status:'password updated'})
 
   } catch(error){
-    res.json({message: "Something went wrong"});
+    res.json({message: 'Something went wrong'});
     console.log(error)
   } 
 });
